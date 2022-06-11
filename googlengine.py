aed07e75b3b3df6721b5f9b038d85f73d34ed0b5
gcpguild <107158741+gcpguild@users.noreply.github.com>
2022-06-11 19:55:41 +0530

    Update googlengine.py

--- googlengine.py
+++ googlengine.py
@@ -90,7 +90,7 @@ bese = re.sub('</h3>', ',', str(bese))
 bese = re.sub('[^A-Za-z|,]+', ' ' , str(bese))
 
 
-mano = [ x.strip() for x in bese.strip('[]').split(',') ]
+mano = sorted([ x.strip() for x in bese.strip('[]').split(',') ])
 
 
 gencsv = pd.DataFrame(mano)
